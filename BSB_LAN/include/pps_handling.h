244e49ba0bb9af9029408b5da7656e093f2c8594
chah90 <Ch.Ahlbrand@gmail.com>
2021-11-09 16:09:52 +0100

    Cleanup

--- BSB_LAN/include/pps_handling.h
+++ BSB_LAN/include/pps_handling.h
@@ -38,37 +38,7 @@ uint16_t pps_bus_handling(byte *msg) {
               tx_msg[7] = 0x00;
               break;
             case 7:
-            {
-//              if (pps_time_set == true) {
-                // bool found = false;
-//                 bool next_active = true;
-//                 int16_t current_time = hour() * 6 + minute() / 10;
-//                 int8_t PPS_weekday = weekday() - 1;
-//                 uint8_t next_switchday = 0;
-//                 uint8_t next_switchtime = 0;
-//                 if (PPS_weekday == 0) PPS_weekday = 7;
-//                 uint8_t index = PPS_S11 + ((PPS_weekday - 1) * 6);
-//                 while (!found) {
-//                   if (current_time < pps_values[index] || (index >= PPS_S11 + ((PPS_weekday - 1 ) * 6) + 6)  || index < PPS_S11 + ((PPS_weekday - 1) * 6)) {
-//                     if (pps_values[index] == 0x90) {
-//                       if (PPS_weekday == 7) PPS_weekday = 0;
-//                       index = PPS_S11 + PPS_weekday * 6;
-//                     }
-//                     next_switchtime = pps_values[index];
-//                     next_switchday = ((index - PPS_S11) / 6) + 1;
-//                     next_switchday = next_switchday + (0x10 * next_active);
-//                     found = true;
-//                   } else {
-//                     index++;
-//                     next_active = !next_active;
-//                   }
-//                   if (index > PPS_E73) index = PPS_S11;
-//                 }
-                  //
-                  // tx_msg[1] = 0x69;
-                  // tx_msg[6] = next_switchday;     // high nibble: current heating program (0x10 = reduced, 0x00 = comfort), low nibble: day of week
-                  // tx_msg[7] = next_switchtime;    // next heating program time (encoded as one increment translates to one 10 minute block)		  
-				
+            {				
 				uint8_t PPS_weekday = pps_values[PPS_DOW] - 1;  
 				if (PPS_weekday == 0) {
 					PPS_weekday = 7;
@@ -78,33 +48,12 @@ uint16_t pps_bus_handling(byte *msg) {
                 tx_msg[6] = PPS_weekday + 0x10;     // high nibble: current heating program (0x10 = reduced, 0x00 = comfort), low nibble: day of week
                 tx_msg[7] = 72;    // next heating program time (encoded as one increment translates to one 10 minute block)
 
-                // if (current_switchday != next_switchday) {    // Set presence parameter to on or off at the beginning of (non-)heating timeslot
-//                   pps_values[PPS_AW] = !next_active;
-//                   current_switchday = next_switchday;
-//                 }
-//
-//                 if (pps_values[PPS_AW] == 0) {                 // Set destination temperature (comfort + knob for heating period, otherwise reduced temperature)
-//                   pps_values[PPS_RTZ] = pps_values[PPS_RTA];
-//                 } else {
-//                   pps_values[PPS_RTZ] = pps_values[PPS_RTS] + pps_values[PPS_PDK];
-//                 }
                 break;
-//              } else {
-//                msg_cycle++;  // If time is not yet set, above code is not executed, but following case will. Increase msg_cycle so that it is not run a second time in the next iteration.
-//              }
+
             }
-//            break;
             case 8:
-              //tx_msg[1] = 0x08;     // Raumtemperatur Soll
-              //tx_msg[6] = pps_values[PPS_RTS] >> 8;
-              //tx_msg[7] = pps_values[PPS_RTS] & 0xFF;
-              //break;
 			  msg_cycle++;
             case 9:
-              //tx_msg[1] = 0x09;     // Raumtemperatur Abwesenheit Soll
-              //tx_msg[6] = pps_values[PPS_RTA] >> 8;
-              //tx_msg[7] = pps_values[PPS_RTA] & 0xFF;
-              //break;
 			  msg_cycle++;
             case 10:
               tx_msg[1] = 0x0B; // Trinkwassertemperatur Soll
@@ -116,103 +65,31 @@ uint16_t pps_bus_handling(byte *msg) {
               tx_msg[7] = pps_values[PPS_AW];
               break;
             case 12:
-              //tx_msg[1] = 0x1E; // Trinkwassertemperatur Reduziert Soll
-              //tx_msg[6] = pps_values[PPS_TWR] >> 8;
-              //tx_msg[7] = pps_values[PPS_TWR] & 0xFF;
-              //break;
 			  msg_cycle++;
             case 13:
             {
-              //if ((pps_time_set == true || pps_wday_set == true) && pps_time_received == true) {
-                //tx_msg[0] = 0xFB; // send time to heater
                 tx_msg[1] = 0x79;
                 tx_msg[4] = pps_values[PPS_DOW]; // day of week
                 tx_msg[5] = hour(); // hour
                 tx_msg[6] = minute(); // minute
                 tx_msg[7] = second(); // second
                 break;
-              //}
             }
             case 14:
-            {
-              //if ((pps_time_set == true || pps_wday_set == true) && pps_time_received == true) {
-               // tx_msg[0] = 0xFE; // unknown telegram
-              //  tx_msg[1] = 0x79;
-              //  pps_time_set = false;
-              //  pps_wday_set = false;
-              //  break;
-              //}
-			 msg_cycle++;	
-            }
+			  msg_cycle++;	
             case 15:
-              //tx_msg[1] = 0x60;
-              //tx_msg[2] = pps_values[PPS_E13];
-              //tx_msg[3] = pps_values[PPS_S13];
-              //tx_msg[4] = pps_values[PPS_E12];
-              //tx_msg[5] = pps_values[PPS_S12];
-              //tx_msg[6] = pps_values[PPS_E11];
-              //tx_msg[7] = pps_values[PPS_S11];
-              //break;
 			  msg_cycle++;
             case 16:
-              //tx_msg[1] = 0x61;
-              //tx_msg[2] = pps_values[PPS_E23];
-              //tx_msg[3] = pps_values[PPS_S23];
-              //tx_msg[4] = pps_values[PPS_E22];
-              //tx_msg[5] = pps_values[PPS_S22];
-              //tx_msg[6] = pps_values[PPS_E21];
-              //tx_msg[7] = pps_values[PPS_S21];
-              //break;
 			  msg_cycle++;
             case 17:
-              //tx_msg[1] = 0x62;
-              //tx_msg[2] = pps_values[PPS_E33];
-              //tx_msg[3] = pps_values[PPS_S33];
-              //tx_msg[4] = pps_values[PPS_E32];
-              //tx_msg[5] = pps_values[PPS_S32];
-              //tx_msg[6] = pps_values[PPS_E31];
-              //tx_msg[7] = pps_values[PPS_S31];
-              //break;
 			  msg_cycle++;
             case 18:
-              //tx_msg[1] = 0x63;
-              //tx_msg[2] = pps_values[PPS_E43];
-              //tx_msg[3] = pps_values[PPS_S43];
-              //tx_msg[4] = pps_values[PPS_E42];
-              //tx_msg[5] = pps_values[PPS_S42];
-              //tx_msg[6] = pps_values[PPS_E41];
-              //tx_msg[7] = pps_values[PPS_S41];
-              //break;
 			  msg_cycle++;
             case 19:
-              //tx_msg[1] = 0x64;
-              //tx_msg[2] = pps_values[PPS_E53];
-              //tx_msg[3] = pps_values[PPS_S53];
-              //tx_msg[4] = pps_values[PPS_E52];
-              //tx_msg[5] = pps_values[PPS_S52];
-              //tx_msg[6] = pps_values[PPS_E51];
-              //tx_msg[7] = pps_values[PPS_S51];
-              //break;
 			  msg_cycle++;
             case 20:
-              //tx_msg[1] = 0x65;
-              //tx_msg[2] = pps_values[PPS_E63];
-              //tx_msg[3] = pps_values[PPS_S63];
-              //tx_msg[4] = pps_values[PPS_E62];
-              //tx_msg[5] = pps_values[PPS_S62];
-              //tx_msg[6] = pps_values[PPS_E61];
-              //tx_msg[7] = pps_values[PPS_S61];
-              //break;
 			  msg_cycle++;
             case 21:
-              //tx_msg[1] = 0x66;
-              //tx_msg[2] = pps_values[PPS_E73];
-              //tx_msg[3] = pps_values[PPS_S73];
-              //tx_msg[4] = pps_values[PPS_E72];
-              //tx_msg[5] = pps_values[PPS_S72];
-              //tx_msg[6] = pps_values[PPS_E71];
-              //tx_msg[7] = pps_values[PPS_S71];
-              //break;
 			  msg_cycle++;
             case 22:
               tx_msg[1] = 0x7C;
@@ -220,11 +97,6 @@ uint16_t pps_bus_handling(byte *msg) {
 			  msg_cycle++;
               break;
             case 23:
-              //tx_msg[1] = 0x1B;                    // Frostschutz- und Maximaltemperatur
-              //tx_msg[4] = pps_values[PPS_SMX] >> 8;
-              //tx_msg[5] = pps_values[PPS_SMX] & 0xFF;
-              //tx_msg[6] = pps_values[PPS_FRS] >> 8;
-              //tx_msg[7] = pps_values[PPS_FRS] & 0xFF;
 			  msg_cycle++;
           }
           msg_cycle++;
